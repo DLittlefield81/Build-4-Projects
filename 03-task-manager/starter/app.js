f6ca2e311e87eea9c4c5130f6477b0b123604c90
Dennis Littlefield <dennis.ah.littlefield@gmail.com>
2023-02-01 18:02:21 -0500

    basic express server started

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,1 +1,22 @@
-console.log('Task Manager App')
+const express = require('express')
+const app = express()
+
+// routes
+app.get('/hello', (req, res) => {
+    res.send('Task Manager App')
+})
+
+// app.get('/api/v1/tasks')         //get all tasks
+// app.post('/api/v1/tasks')        //post new tasks
+// app.get('/api/v1/tasks/:id')      //get single task
+// app.patch('/api/v1/tasks/:id')    //update task
+// app.delete('/api/v1/tasks/:id')   //delete task
+
+const port = 3000
+
+app.listen(port, console.log(`Task Manager App running on port ${port}`))
+
+
+
+
+
